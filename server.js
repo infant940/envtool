c2b15d09ce006306774c91804b90f770777c4635
infant940 <michaelinfant940@gmail.com>
2019-12-14 18:50:52 +0530

    server post

--- server.js
+++ server.js
@@ -29,12 +29,29 @@ app.get("/envs", (req, res) => {
     });
 });
 
+app.post("/sample", (req, res) => {
+  dbo
+    .collection("infant")
+    .find()
+    .toArray((err, data ) => {
+      console.log(data);
+       var array1 = req.body.data.split("\n")
+       var array2 =[];
+      data.forEach(function(item) {
+        array1.forEach(function(item1){
+          if(item.name == item1)
+            array2.push(item);
+        })
+       })
+      res.send(array2);
+    });
+});
+
 app.post("/envs", function(req, res) {
   var myobj = {
-      "name": req.body.name,
-      "value":req.body.value
-    }
-  
+    name: req.body.name,
+    value: req.body.value
+  };
   dbo.collection("infant").insertOne(myobj, function(err, obj) {
     if (err) throw err;
     console.log("inserted");
@@ -44,8 +61,8 @@ app.post("/envs", function(req, res) {
 
 app.delete("/envs/:name", function(req, res) {
   var myquery = {
-    "name": req.params.name,
- };
+    name: req.params.name
+  };
   dbo.collection("infant").deleteOne(myquery, function(err, obj) {
     if (err) throw err;
     console.log("deleted");
@@ -55,15 +72,18 @@ app.delete("/envs/:name", function(req, res) {
 
 app.put("/envs", function(req, res) {
   var myquery = {
-    "name": req.body.update
+    name: req.body.update
   };
   var newvalues = {
     $set: {
-      "name": req.body.name,
-      "value":req.body.value
-    },
+      name: req.body.name,
+      value: req.body.value
+    }
   };
-  console.log("Name" + req.body.name + ", Update" + req.body.update, + "value" + req.body.name + ", Update" + req.body.update);
+  console.log(
+    "Name" + req.body.name + ", Update" + req.body.update,
+    +"value" + req.body.name + ", Update" + req.body.update
+  );
   dbo.collection("infant").updateOne(myquery, newvalues, function(err, data) {
     if (err) {
       res.send({ error: err });
@@ -72,5 +92,6 @@ app.put("/envs", function(req, res) {
     }
   });
 });
+
 
 app.listen(4000, "localhost", () => console.log("Running"));
